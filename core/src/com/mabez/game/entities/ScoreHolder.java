bca428da32c572ffa1616dcfbf59740f7ddcc858
MabezDev <mabez123@gmail.com>
2014-05-18 14:07:34 +0100

    added template for score holding system

---
+++ core/src/com/mabez/game/entities/ScoreHolder.java
@@ -0,0 +1,17 @@
+package com.mabez.game.entities;
+
+import java.io.Serializable;
+
+/**
+ * Created by user on 18/05/2014.
+ */
+public class ScoreHolder implements Serializable {
+
+    //will be serialize with top ten scores
+    //will generate empty ones to fill gaps
+    // i.e player = -----
+    //will be loaded on splash screen
+    //data from here will be displayed in HighScoresState
+    //Sort form highest to lowest top 10, using bubble sort or something
+
+}
--- core/src/com/mabez/game/gamestate/HighScoresState.java
+++ core/src/com/mabez/game/gamestate/HighScoresState.java
@@ -1,7 +1,38 @@
 package com.mabez.game.gamestate;
 
+import com.mabez.game.managers.SceneManager;
+
 /**
  * Created by user on 18/05/2014.
  */
-public class HighScoresState {
+public class HighScoresState extends BaseState{
+
+    public HighScoresState(SceneManager sm) {
+        super(sm);
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void draw() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void HandleInput() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
